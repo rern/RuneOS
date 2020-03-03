6e1da249e837fb832daa81b3fac0e86ab604d57c
rern <rernrern@gmail.com>
2020-03-03 13:57:05 +0000

    Update create-rune.sh

--- usr/local/bin/create-rune.sh
+++ usr/local/bin/create-rune.sh
@@ -231,7 +231,7 @@ systemctl enable $startup
 
 #---------------------------------------------------------------------------------
 # data - settings directories
-/srv/http/bash/resetdata.sh
+/srv/http/bash/resetdata.sh "$version"
 
 # addons
 wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/addons-list.php -P /srv/http
