fb2082b034e21bb3a8a7e221f2d0270c4c6e9298
rern <rernrern@gmail.com>
2020-03-03 12:20:01 +0700

    Update create-rune.sh

--- usr/local/bin/create-rune.sh
+++ usr/local/bin/create-rune.sh
@@ -124,12 +124,13 @@ echo -e "\n\e[36mInstall customized packages and web interface ...\e[m\n"
 
 wget -q --show-progress https://github.com/rern/RuneOS/archive/master.zip
 bsdtar xvf *.zip --strip 1 --exclude=.* --exclude=*.md -C /
+rm master.zip
 
 # ui
 ############### NOTE: change UPDATE.zip to master.zip after merge
 wget -qN https://github.com/rern/RuneAudio-Re2/archive/UPDATE.zip
-bsdtar --strip 1 --exclude=*.* --exclude=./etc --exclude=./usr -C / -xf master.zip
-rm master.zip
+bsdtar --strip 1 --exclude=*.* --exclude=./etc --exclude=./usr -C / -xf UPDATE.zip
+rm UPDATE.zip
 
 chmod 755 /srv/http/* /srv/http/bash/* /srv/http/settings/* /usr/local/bin/*
 chown -R http:http /srv/http
