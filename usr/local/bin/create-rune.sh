362110102dd32d38c66d21b7d01d1cbe1497d7e3
rern <rernrern@gmail.com>
2020-05-20 21:37:39 +0700

    Update create-rune.sh

--- usr/local/bin/create-rune.sh
+++ usr/local/bin/create-rune.sh
@@ -196,7 +196,7 @@ fi
 ( crontab -l &> /dev/null; echo '00 01 * * * /srv/http/addons-update.sh &' ) | crontab -
 
 # fstab - allow write
-chmod 666 /etc/fstab
+chown http:http  /etc/fstab
 
 # lvm - remove invalid value
 #sed -i '/event_timeout/ s/^/#/' /usr/lib/udev/rules.d/11-dm-lvm.rules
