e76a7cc16e3323af999d1789ef6001c27fa8784e
rern <rernrern@gmail.com>
2020-06-17 08:39:08 +0000

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -90,6 +90,8 @@ ROOT: \Z1$ROOT\Z0"
 		4) file=ArchLinuxARM-rpi-4-latest.tar.gz ;;
 	esac
 	
+	[[ $rpi != 0 ]] && rpiname=$rpi || rpiname=Zero
+	
 	yesno '\Z1Connect Wi-Fi on boot?\Z0'
 	if [[ $? == 0 ]]; then
 		ssid=$( inputbox '\Z1Wi-Fi\Z0 - SSID:' $ssid )
@@ -117,7 +119,7 @@ ROOT: \Z1$ROOT\Z0"
 	yesno "\Z1Confirm data:\Z0\n\n\
 BOOT path : \Z1$BOOT\Z0\n\
 ROOT path : \Z1$ROOT\Z0\n\
-Target    : \Z1Raspberry Pi $rpi\Z0\n\
+Target    : \Z1Raspberry Pi $rpiname\Z0\n\
 $wifi"
 	[[ $? == 1 ]] && getData
 }
@@ -232,7 +234,7 @@ chmod 755 $ROOT/usr/local/bin/*.sh
 
 [[ $rpi == 0 ]] && rpi=Zero
 msgbox "
-        Arch Linux Arm for \Z1Raspberry Pi $rpi\Z0\n\
+        Arch Linux Arm for \Z1Raspberry Pi $rpiname\Z0\n\
                created successfully.
 " 8 58
 
