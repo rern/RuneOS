24c7d7bebfc540679a1094e5424389c22581362a
rern <rernrern@gmail.com>
2020-06-23 12:21:07 +0700

    update

--- boot/x.sh
+++ boot/x.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm $0
+(( $( sfdisk -F /dev/mmcblk0 | head -n1 | awk '{print $6}' ) == 0 )) && exit
+
 echo -e "d\n\nn\n\n\n\n\nw" | fdisk /dev/mmcblk0 &>/dev/null
 partprobe /dev/mmcblk0
 resize2fs /dev/mmcblk0p2
