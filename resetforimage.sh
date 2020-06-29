261070a20560ed349fe633fa04e8df86f367e4e7
rern <rernrern@gmail.com>
2020-06-29 18:19:25 +0700

    Update resetforimage.sh

--- resetforimage.sh
+++ resetforimage.sh
@@ -18,12 +18,12 @@ select=" $select "
 
 if [[ $select == *' 1 '* ]]; then
 	string=$( cat <<'EOF'
-	#!/bin/bash
-	rm $0
-	(( $( sfdisk -F /dev/mmcblk0 | head -n1 | awk '{print $6}' ) == 0 )) && exit
-	echo -e "d\n\nn\n\n\n\n\nw" | fdisk /dev/mmcblk0 &>/dev/null
-	partprobe /dev/mmcblk0
-	resize2fs /dev/mmcblk0p2
+#!/bin/bash
+rm $0
+(( $( sfdisk -F /dev/mmcblk0 | head -n1 | awk '{print $6}' ) == 0 )) && exit
+echo -e "d\n\nn\n\n\n\n\nw" | fdisk /dev/mmcblk0 &>/dev/null
+partprobe /dev/mmcblk0
+resize2fs /dev/mmcblk0p2
 EOF
 )
 	echo "$string" > /boot/x.sh
