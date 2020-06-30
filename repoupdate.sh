24996008ee29573305ef4d734d44983e53a76d08
rern <rernrern@gmail.com>
2020-06-30 16:11:44 +0700

    Update repoupdate.sh

--- repoupdate.sh
+++ repoupdate.sh
@@ -2,19 +2,11 @@
 
 rm $0
 
-arch=$( dialog --colors --output-fd 1 --menu '\n\Z1Arch:\Z0' 8 30 0 \
-	0 'armv6h' \
-	1 'armv7h' )
-[[ $arch == 0 ]] && arch=armv6h || arch=armv7h
-ip=$( dialog --colors --output-fd 1 --inputbox "\n\Z1Local Git IP:\Z0" 10 30 192.168.1.9 )
+updateRepo() {
+	cd /mnt/Git/rern.github.io/$1
 
-mkdir -p /mnt/Git
-mount -t cifs -o password= //$ip/Git /mnt/Git
-currentdir=$( pwd )
-cd /mnt/Git/rern.github.io/$arch
-
-# index.html
-html='<!DOCTYPE html>
+	# index.html
+	html='<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -27,25 +19,40 @@ html='<!DOCTYPE html>
 <body>
 <table>
 	<tr><td><a href="/">../</a></td><td></td></tr>'
-pkg=( $( ls *.pkg.tar.xz ) )
-sizedate=$( ls -lh *.pkg.tar.xz | cut -c24-40 )
-readarray -t sizedate <<<"$sizedate"
-pkgL=${#pkg[@]}
-for (( i=1; i < $pkgL; i++ )); do
-	pkg=${pkg[$i]}
+	pkg=( $( ls *.pkg.tar.xz ) )
+	sizedate=$( ls -lh *.pkg.tar.xz | cut -c24-40 )
+	readarray -t sizedate <<<"$sizedate"
+	pkgL=${#pkg[@]}
+	for (( i=1; i < $pkgL; i++ )); do
+		pkg=${pkg[$i]}
+		html+='
+	<tr><td><a href="'$1'/'$pkg'">'$pkg'</a></td><td>'"${sizedate[$i]}"'</td></tr>'
+	done
 	html+='
-	<tr><td><a href="'$arch'/'$pkg'">'$pkg'</a></td><td>'"${sizedate[$i]}"'</td></tr>'
-done
-html+='
 <table>
 </body>
 </html>'
 
-echo -e "$html" > ../$arch.html
+	echo -e "$html" > ../$1.html
 
-# recreate database
-rm RR*
-repo-add -R RR.db.tar.xz *.xz
+	# recreate database
+	rm RR*
+	repo-add -R RR.db.tar.xz *.xz
+}
+
+arch=$( dialog --colors --output-fd 1 --checklist '\n\Z1Arch:\Z0' 8 30 0 \
+	1 armv6h on \
+	2 armv7h on )
+select=" $select "
+
+ip=$( dialog --colors --output-fd 1 --inputbox "\n\Z1Local Git IP:\Z0" 10 30 192.168.1.9 )
+
+mkdir -p /mnt/Git
+mount -t cifs -o password= //$ip/Git /mnt/Git
+currentdir=$( pwd )
+
+[[ $select == *' 1 '* ]] && updateRepo armv6h
+[[ $select == *' 2 '* ]] && updateRepo armv7h
 
 cd "$currentdir"
 umount -l /mnt/Git
