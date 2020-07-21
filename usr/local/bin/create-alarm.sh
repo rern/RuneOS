449e0f4182b9b00f3c6a8a2dcc3e16caf9b5fedf
rern <rernrern@gmail.com>
2020-07-21 10:14:07 +0000

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -151,7 +151,7 @@ else
 	( wget -O $file http://os.archlinuxarm.org/os/$file 2>&1 \
 		| stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { \
 			print "XXX\n"substr($0,63,3)
-			print "\\n\\Z1Download Arch Linux Arm\\Z0\\n"
+			print "\\nDownload \\Z1Arch Linux Arm\\Z0\\n"
 			print "Time left: "substr($0,74,5)"\nXXX" }' ) \
 		| dialog "${opt[@]}" --gauge "\n
 Connecting ...
