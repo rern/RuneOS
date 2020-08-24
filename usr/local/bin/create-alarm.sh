44acc2be0c0cad5135b78d8a30d082f9af31e5a4
rern <rernrern@gmail.com>
2020-08-24 11:31:08 +0700

    Update create-alarm.sh

--- usr/local/bin/create-alarm.sh
+++ usr/local/bin/create-alarm.sh
@@ -383,4 +383,4 @@ clear
 
 cat /dev/zero | ssh-keygen -q -N "" &> /dev/null
 ssh-keygen -R $rpiip &> /dev/null
-sshpass -p root ssh root@$rpiip
+sshpass -p root ssh -o StrictHostKeyChecking=no root@$rpiip
