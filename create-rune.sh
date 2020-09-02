eb45f5748fddfa23c0772dfa9e52f516c7a18edd
rern <rernrern@gmail.com>
2020-09-02 10:54:36 +0700

    Update create-rune.sh

--- create-rune.sh
+++ create-rune.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# create-rune.sh any - custom inputs for version and branch
 # new version:
 #1   - https://github.com/rern/RuneAudio_Addons/blob/master/addons-list.json > new item with key > "rreN": {
 #2   - this file > version=eN
@@ -8,11 +7,15 @@ version=e5
 uibranch=master
 addonalias=rr$version
 
-trap 'rm -f /var/lib/pacman/db.lck; clear; exit' INT
+trap 'rm -f /var/lib/pacman/db.lck; exit' INT
 
 hardwarecode=$( grep Revision /proc/cpuinfo )
 hwcode=${hardwarecode: -3:2}
-[[ $hwcode =~ ^(00|01|02|03|04|09)$ ]] && nowireless=1
+if [[ $hwcode =~ ^(00|01|02|03|04|09)$ ]]; then
+	nowireless=1
+	sed -i '/disable-wifi\|disable-bt\|bcmbt/ d' /boot/config.txt
+	sed -i 's/bluez bluez-alsa-git bluez-utils //' /boot/features
+fi
 [[ ${hardwarecode: -4:1} == 0 ]] && rpi01=1
 
 cols=$( tput cols )
@@ -34,6 +37,7 @@ if ! grep -q '^\[RR\]' /etc/pacman.conf; then
 [RR]\
 SigLevel = Optional TrustAll\
 Server = https://rern.github.io/$arch\
+
 ' /etc/pacman.conf
 fi
 
@@ -42,118 +46,21 @@ optbox=( --colors --no-shadow --no-collapse )
 opt=( --backtitle "$title" ${optbox[@]} )
 
 dialog "${optbox[@]}" --infobox "
+
                 \Z1RuneAudio+R $version\Z0
 " 7 50
 sleep 3
-
-# package mirror server
-readarray -t lines <<< "$( grep . /etc/pacman.d/mirrorlist | sed -n '/### A/,$ p' | sed 's/ (not Austria\!)//' )"
-clist=( 0 'Auto - By Geo-IP' )
-url=( '' )
-i=0
-for line in "${lines[@]}"; do
-	if [[ ${line:0:4} == '### ' ]];then
-		city=
-		country=${line:4}
-	elif [[ ${line:0:3} == '## ' ]];then
-		city=${line:3}
-	else
-		[[ -n $city ]] && cc="$country - $city" || cc=$country
-		(( i++ ))
-		clist+=( $i "$cc" )
-		url+=( $( sed 's|.*//\(.*\).mirror.*|\1|' <<< $line ) )
-	fi
-done
-
-code=$( dialog "${opt[@]}" --output-fd 1 --menu "
-\Z1Package mirror server:\Z0
-" 0 0 0 \
-"${clist[@]}" )
-
-clear
-
-[[ -n $code ]] && sed -i '/^Server/ s|//.*mirror|//'${url[$code]}'.mirror|' /etc/pacman.d/mirrorlist
 
 # dialog package
 pacman -Sy --noconfirm --needed dialog
 
 #----------------------------------------------------------------------------
-if (( $# > 0 )); then
-	version=$( dialog --colors --output-fd 1 --inputbox "\n\Z1Version:\Z0" 0 0 $version )
-	uibranch=$( dialog --colors --output-fd 1 --inputbox "\n\Z1UI branch:\Z0" 0 0 $uibranch )
-	addonalias=rr$version
-fi
-
-    bluez='\Z1Bluez\Z0     - Bluetooth supports'
- chromium='\Z1Chromium\Z0  - Browser on RPi'
-  hostapd='\Z1hostapd\Z0   - RPi access point'
-      kid='\Z1Kid3\Z0      - Metadata tag editor'
-    samba='\Z1Samba\Z0     - File sharing'
-shairport='\Z1Shairport\Z0 - AirPlay renderer'
- snapcast='\Z1Snapcast\Z0  - Synchronous multiroom player'
-  spotify='\Z1Spotifyd\Z0  - Spotify renderer'
- upmpdcli='\Z1upmpdcli\Z0  - UPnP renderer'
-
-if [[ $nowireless ]]; then
-	bluez='Bluez     - (no onboard)'
-	onoffbluez=off
-else
-	onoffbluez=on
-fi
-if [[ $rpi01 ]]; then
-	chromium='Chromium  - (not for RPi Zero, 1)'
-	onoffchromium=off
-else
-	onoffchromium=on
-fi
-
-selectFeatures() { # --checklist <message> <lines exclude checklist box> <0=autoW dialog> <0=autoH checklist>
-	select=$( dialog "${opt[@]}" --output-fd 1 --checklist "
-\Z1Select features to install:
-\Z4[space] = Select / Deselect\Z0
-" 9 0 0 \
-1 "$bluez" $onoffbluez \
-2 "$chromium" $onoffchromium \
-3 "$hostapd" on \
-4 "$kid" on \
-5 "$samba" on \
-6 "$shairport" on \
-7 "$snapcast" on \
-8 "$spotify" on \
-9 "$upmpdcli" on )
-	
-	select=" $select "
-	[[ $select == *' 1 '* && ! $nowireless ]] && features+='bluez bluez-alsa-git bluez-utils ' && list+="$bluez"$'\n'
-	[[ $select == *' 2 '* && ! $rpi01 ]] && features+='chromium matchbox-window-manager upower xf86-video-fbdev xf86-video-vesa xorg-server xorg-xinit ' && list+="$chromium"$'\n'
-	[[ $select == *' 3 '* ]] && features+='dnsmasq hostapd ' && list+="$hostapd"$'\n'
-	[[ $select == *' 4 '* ]] && features+='kid3-cli ' && list+="$kid"$'\n'
-	[[ $select == *' 5 '* ]] && features+='samba ' && list+="$samba"$'\n'
-	[[ $select == *' 6 '* ]] && features+='shairport-sync ' && list+="$shairport"$'\n'
-	[[ $select == *' 7 '* ]] && features+='snapcast ' && list+="$snapcast"$'\n'
-	[[ $select == *' 8 '* ]] && features+='spotifyd ' && list+="$spotify"$'\n'
-	[[ $select == *' 9 '* ]] && features+='upmpdcli ' && list+="$upmpdcli"$'\n'
-	echo $features > /tmp/features
-	echo -e "$list" > /tmp/list
-}
-if [[ ! -e /tmp/features ]]; then
-	selectFeatures
-else
-	features=$( cat /tmp/features )
-	list=$( cat /tmp/list )
-fi
-
-dialog "${opt[@]}" --yesno "
-Confirm features to install:
-$list
-" 0 0
-
-clear
-
-#----------------------------------------------------------------------------
 pacmanFailed() {
 	dialog "${opt[@]}" --msgbox "
 $1
+
 Run \Z1create-rune.sh\Z0 again.
+
 " 0 0
 	exit
 }
@@ -168,7 +75,7 @@ packages+='nfs-utils nginx-mainline-pushstream nss-mdns ntfs-3g parted php-fpm s
 
 echo -e "\n\e[36mInstall packages ...\e[m\n"
 
-pacman -S --noconfirm --needed $packages $features
+pacman -S --noconfirm --needed $packages $( cat /root/features )
 [[ $? != 0 ]] && pacmanFailed 'Packages download incomplete!'
 
 echo -e "\n\e[36mInstall configurations and web interface ...\e[m\n"
@@ -185,8 +92,6 @@ cp -r /tmp/config/* /
 
 #---------------------------------------------------------------------------------
 echo -e "\n\e[36mConfigure ...\e[m\n"
-
-[[ $nowireless ]] && sed -i '/disable-wifi\|disable-bt\|bcmbt/ d' /boot/config.txt
 
 # RPi 4 - rename bluetooth file
 [[ $hwcode == 11 ]] && mv /usr/lib/firmware/updates/brcm/BCM{4345C0,}.hcd
@@ -239,7 +144,7 @@ sed -i '/^-.*pam_systemd/ s/^-/#/' /etc/pam.d/system-login
 # password
 echo root:rune | chpasswd
 [[ -e /usr/bin/smbd ]] && ( echo rune; echo rune ) | smbpasswd -s -a root
-sed -i 's/\(PermitEmptyPasswords \).*/#\1no' /etc/ssh/sshd_config
+sed -i 's/\(PermitEmptyPasswords \).*/#\1no/' /etc/ssh/sshd_config
 
 # no samba
 [[ ! -e /usr/bin/samba ]] && rm -rf /etc/samba /etc/systemd/system/wsdd.service /usr/local/bin/wsdd.py
@@ -284,7 +189,7 @@ systemctl enable $startup
 /srv/http/bash/data-reset.sh "$version"
 
 # remove files and package cache
-rm /etc/motd /root/create-rune.sh /var/cache/pacman/pkg/*
+rm /boot/features /etc/motd /root/create-rune.sh /var/cache/pacman/pkg/*
 
 # usb boot - disable sd card polling
 ! df | grep -q /dev/mmcblk0 && echo 'dtoverlay=sdtweak,poll_once' >> /boot/config.txt
@@ -295,7 +200,9 @@ if [[ $rpi01 && $features =~ upmpdcli ]]; then
 fi
 
 dialog "${optbox[@]}" --msgbox "
+
     \Z1RuneAudio+R $version\Z0 created successfully.
+
             Press \Z1Enter\Z0 to reboot
 " 10 50
 
