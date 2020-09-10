9097fc2a9c0a349385e5aeb3f942a9e76e789907
rern <rernrern@gmail.com>
2020-09-10 20:10:59 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -14,7 +14,7 @@ mkdir -p /mnt/{BOOT,ROOT}
 mount ${dev}1 /mnt/BOOT
 mount ${dev}2 /mnt/ROOT
 
-if [[ $( df -T /mnt/BOOT | tail -1 | awk '{print $2$3}' ) != vfat100M ]]; then
+if [[ $( df -Th /mnt/BOOT | tail -1 | awk '{print $2$3}' ) != vfat100M ]]; then
 	echo ${dev}1 not BOOT partition
 	exit
 fi
