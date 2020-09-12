ea02705e620f457375d28c0d7aa5ee1508e749ab
rern <rernrern@gmail.com>
2020-09-12 09:13:46 +0700

    Create reset.sh

---
+++ reset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ ! -e /srv/http/data/addons ]]; then
+	echo This is not RuneAudio+R
+	exit
+fi
+
+optbox=( --colors --no-shadow --no-collapse )
+
+dialog "${optbox[@]}" --infobox "
+
+                   \Z1Reset RuneAudio+R\Z0
+                          for
+                      Image File				   
+" 9 58
+sleep 3
+
+routerip=$( ip r get 1 | head -1 | cut -d' ' -f3 )
+subip=${routerip%.*}.
+
+rpiip=$( dialog "${opt[@]}" --output-fd 1 --inputbox "
+\Z1Raspberry Pi IP:\Z0
+" 0 0 $subip )
+
+sshpass -p rune ssh root@$rpiip
+
+bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/imagereset.sh )
