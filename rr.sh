004e5700679de3ff4ed5bf8f520025afeff9a4a2
rern <rernrern@gmail.com>
2020-09-15 08:07:38 +0700

    Update rr.sh

--- rr.sh
+++ rr.sh
@@ -9,3 +9,6 @@ reset() {
 image() {
 	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/imagecreate.sh )
 }
+repo
+	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/repoupdate.sh )
+}
