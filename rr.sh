c7b7ca4a6f5edd5e854c9a6464be227d5b4cb5f6
rern <rernrern@gmail.com>
2020-09-21 11:28:33 +0700

    Update rr.sh

--- rr.sh
+++ rr.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-create() {
+optbox=( --colors --no-shadow --no-collapse )
+
+dialog "${optbox[@]}" --infobox "
+
+
+                     \Z1RuneAudio+R\Z0
+
+" 9 58
+sleep 3
+
+cmd=$( dialog "${optbox[@]}" --output-fd 1 --menu "
+\Z1Command:\Z0
+" 8 0 0 \
+1 'Create RuneAudio+R' \
+2 'Reset RuneAudio+R' \
+3 'Image RuneAudio+R' \
+4 'Package Repo Update' )
+
+case $cmd in
+
+create )
 	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/create.sh )
-}
-reset() {
+	;;
+reset )
 	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/reset.sh )
-}
-image() {
+	;;
+image )
 	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/imagecreate.sh )
-}
-repo
+	;;
+repo )
 	bash <( wget -qO - https://github.com/rern/RuneOS/raw/master/repoupdate.sh )
-}
+	;;
+
+esac
