8e6dd50e6abd0bd882fbf30c2b9cdafd3c5578ca
rern <rernrern@gmail.com>
2020-10-18 07:37:54 +0700

    Update motd.sh

--- etc/profile.d/motd.sh
+++ etc/profile.d/motd.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo -e "\e[38;5;45m$( < /etc/motd.logo )\e[0m\n"
-
+clear
+echo -e "\e[38;5;6m$( < /etc/motd.logo )\e[0m
+R+R $( cat /srv/http/data/system/version )"
+printf "\e[38;5;45m%*s\e[0m\n" $( tput cols ) | tr ' ' -
