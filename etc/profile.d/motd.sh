a585987ab18801c69dffd459696249ae9022a23c
rern <rernrern@gmail.com>
2020-10-18 20:45:09 +0700

    Add files via upload

--- etc/profile.d/motd.sh
+++ etc/profile.d/motd.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 clear
 echo -e "\e[38;5;6m\
-         .:kXWMMMWKkdolcclkMMMM:        
-        ;WMMMXx?'''        KMMM:        
-        :MMN'              xMMM.        
-        .WMMc             :0MMM         
-         dMMW;      ,     :WMMM         
-         .NMMWxdxkK0;     'NMMM.        
-          cMMMMWKx;:      'kMMM.        
-           :lNNl''   ,     oMMM:        
-                  .oK;     xMMM,        
-              .unWMNc     .NMMd         
-\e[0m\
-R+R $( cat /srv/http/data/system/version )"
-printf "\e[38;5;8m%*s\e[0m\n" $( tput cols ) | tr ' ' -
+ .:kXWMMMWKkdolcclkMMMM:
+;WMMMXx?'''        KMMM:
+:MMN'              xMMM.
+.WMMc             :0MMM
+ dMMW;      ,     :WMMM
+ .NMMWxdxkK0;     'NMMM.
+  cMMMMWKx;:      'kMMM.
+   :lNNl''   ,     oMMM:
+          .oK;     xMMM,
+      .unWMNc     .NMMd
+       ':do:'     kMMk'
+                .kMMx'\
+\e[0m"
+echo -e "\e[38;5;8m-------------------------\e[0m\n"
