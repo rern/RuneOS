01b272471beb367ed297db8f9b31d47ff18285f7
rern <rernrern@gmail.com>
2020-10-18 07:44:15 +0700

    u

--- etc/profile.d/motd.sh
+++ etc/profile.d/motd.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 clear
-echo -e "\e[38;5;6m$( < /etc/motd.logo )\e[0m
+echo -e "\e[38;5;6m\
+         .:kXWMMMWKkdolcclkMMMM:        
+        ;WMMMXx?'''        KMMM:        
+        :MMN'              xMMM.        
+        .WMMc             :0MMM         
+         dMMW;      ,     :WMMM         
+         .NMMWxdxkK0;     'NMMM.        
+          cMMMMWKx;:      'kMMM.        
+           :lNNl''   ,     oMMM:        
+                  .oK;     xMMM,        
+              .unWMNc     .NMMd         
+\e[0m\
 R+R $( cat /srv/http/data/system/version )"
-printf "\e[38;5;45m%*s\e[0m\n" $( tput cols ) | tr ' ' -
+printf "\e[38;5;8m%*s\e[0m\n" $( tput cols ) | tr ' ' -
