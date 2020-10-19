d41327a68df5a904bb3e8668e1145bc9f6674e3d
rern <rernrern@gmail.com>
2020-10-19 07:54:59 +0700

    u

--- etc/profile.d/motd.sh
+++ etc/profile.d/motd.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 clear
-echo -e "\e[38;5;6m\
- .:kXWMMMWKkdolcclkMMMM:
-;WMMMXx?'''        KMMM:
-:MMN'              xMMM.
-.WMMc             :0MMM
- dMMW;      ,     :WMMM
- .NMMWxdxkK0;     'NMMM.
-  cMMMMWKx;:      'kMMM.
-   :lNNl''   ,     oMMM:
-          .oK;     xMMM,
-      .unWMNc     .NMMd
-       ':do:'     kMMk'
-                .kMMx'\
-\e[0m"
-echo -e "\e[38;5;8m-------------------------\e[0m\n"
+echo -e "\e[36m\e[48;5;236m\
+ .:kXWMMMWKkdolcclkMMMM:   
+;WMMMXx?'''        KMMM:   
+:MMN'              xMMM.   
+.WMMc             :0MMM    
+ dMMW;      ,     :WMMM    
+ .NMMWxdxkK0;     'NMMM.   
+  cMMMMWKx;:      'kMMM.   
+   :lNNl''   ,     oMMM:   
+          .oK;     xMMM,   
+      .unWMNc     .NMMd    
+       ':do:'     kMMk'    
+                .kMMx'     \
+\e[0m
+"
